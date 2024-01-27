4b8cbdca504551c9eb5771b64a094b1d08f3e2f1
Jeremy Callinan <jeremy.callinan@gmail.com>
2024-01-27 16:04:28 -0500

    GameManager addition

---
+++ Assets/GameManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public int CurrentStepInQuest;
+    public string[] questSteps;
+    public GameObject[] characterPrefabs;
+    public GameObject currentCharacter;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        CurrentStepInQuest = 0;
+        
+    }
+    public void AdvanceQuest()
+    {
+        if (CurrentStepInQuest < questSteps.Length - 1)
+        {
+            CurrentStepInQuest++;
+            // Execute the logic for the new quest step
+            ProcessQuestStep(CurrentStepInQuest);
+        }
+    }
+    // Function to spawn a random character
+    public void SpawnRandomCharacter()
+    {
+        if (characterPrefabs.Length == 0)
+        {
+            Debug.LogWarning("No character prefabs assigned.");
+            return;
+        }
+
+        int randomIndex = Random.Range(0, characterPrefabs.Length);
+        GameObject randomCharacter = characterPrefabs[randomIndex];
+
+        Instantiate(randomCharacter, Vector3.zero, Quaternion.identity); // Spawns at position (0,0,0)
+    }
+    private void ProcessQuestStep(int stepIndex)
+    {
+        // Check if the step index is within bounds of the array
+        if (stepIndex >= 0 && stepIndex < questSteps.Length)
+        {
+            // Load the scene specified in questSteps[stepIndex]
+            SceneManager.LoadScene(questSteps[stepIndex]);
+        }
+        else
+        {
+            Debug.LogError("Quest step index is out of bounds.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
